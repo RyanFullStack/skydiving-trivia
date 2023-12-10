23715c577806ccb02ddeeab569acc666a26a348c
Ryan <ryanjerickson@gmail.com>
2023-12-10 17:52:45 -0600

    ready for questions

--- src/Data/questionList.js
+++ src/Data/questionList.js
@@ -1,6 +1,6 @@
 export const questions = [
-    {questionId: 1, question: 'How many jumps to get your A license?', correctAnswer: '25', incorrectAnswers: ['50', '75', '100'], difficulty: 'Easy'},
-    {questionId: 2, question: 'How many jumps to get your B license?', correctAnswer: '50', incorrectAnswers: ['25', '75', '100'], difficulty: 'Easy'},
-    {questionId: 3, question: 'How many jumps to get your C license?', correctAnswer: '250', incorrectAnswers: ['25', '75', '100'], difficulty: 'Medium'},
-    {questionId: 4, question: 'How many jumps to get your D license?', correctAnswer: '500', incorrectAnswers: ['25', '75', '100'], difficulty: 'Hard'}
+    {questionId: 1, question: 'How many jumps minimum to get your A license?', correctAnswer: '25', incorrectAnswers: ['50', '75', '100'], difficulty: 'Easy'},
+    {questionId: 2, question: 'How many jumps minimum to get your B license?', correctAnswer: '50', incorrectAnswers: ['25', '75', '100'], difficulty: 'Easy'},
+    {questionId: 3, question: 'How many jumps minimum to get your C license?', correctAnswer: '200', incorrectAnswers: ['300', '250', '100'], difficulty: 'Easy'},
+    {questionId: 4, question: 'How many jumps minimum to get your D license?', correctAnswer: '500', incorrectAnswers: ['250', '300', '1000'], difficulty: 'Easy'}
 ]
