88495911f7ed25f30b5fcb1a9a527b9080e1787b
Ryan <ryanjerickson@gmail.com>
2023-12-10 23:29:08 -0600

    dev test removed

--- src/Components/Game/index.jsx
+++ src/Components/Game/index.jsx
@@ -194,7 +194,7 @@ const TriviaQuiz = ({ questions }) => {
       )}
       {!currentQuestion && currentQuestionIndex !== -1 && gameStarted && (
         <EndScreen
-          correctAnswersCount={30}
+          correctAnswersCount={correctAnswersCount}
           questionsList={questionsList}
           selectedDifficulty={selectedDifficulty}
           resetGame={resetGame}
