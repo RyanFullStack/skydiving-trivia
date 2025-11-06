6c35924c846215876bed3adde7db635228ef5155
Ryan <ryanjerickson@gmail.com>
2025-11-05 20:32:33 -0600

    abt updt

--- src/Components/About/index.jsx
+++ src/Components/About/index.jsx
@@ -13,11 +13,9 @@ function About() {
       <p>
         You will be given random questions (50 on Hard, 30 on Medium, 10 on Easy) from a huge dataset of skydiving
         trivia. There are Easy, Medium, and Hard level questions. You will be given questions from your selected difficulty and below.
-        You have 30 seconds for each question. At the end of the game,
-        if you've made it to the leaderboard you'll be prompted to enter your
-        name. Leaderboard status is only given if completed on Hard mode.
+        You have 30 seconds for each question.
       </p>
-      <p className='blue'>Thanks for playing!</p>
+      <p className='blue'>Good Luck and Thanks for playing!</p>
       <button onClick={handleHome}>Home</button>
       <div>
         <small>By: <a href='https://github.com/RyanFullStack' rel='noreferrer' target='_blank'>Ryan Erickson</a></small>
